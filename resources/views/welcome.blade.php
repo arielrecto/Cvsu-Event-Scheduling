3211e08781660ce976bfcc0850115adf66d025cf
arielrecto <arielrecto29@gmail.com>
2024-03-27 20:45:31 +0800

    update:errrors display fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
             <input type="email" name="email" class="w-full input input-accent" placeholder="admin@admin.com">
 
             @if ($errors->has('email'))
-                <p class="text-xs text-error">{{ $errors->frist('email') }}</p>
+                <p class="text-xs text-error">{{ $errors->first('email') }}</p>
             @endif
         </div>
 
@@ -17,7 +17,7 @@
             <label for="" class="text-base text-accent">Password</label>
             <input type="password" name="password" class="w-full input input-accent" placeholder="password">
             @if ($errors->has('password'))
-                <p class="text-xs text-error">{{ $errors->frist('password') }}</p>
+                <p class="text-xs text-error">{{ $errors->first('password') }}</p>
             @endif
         </div>
         <button class="btn btn-accent text-primary ">Login</button>
