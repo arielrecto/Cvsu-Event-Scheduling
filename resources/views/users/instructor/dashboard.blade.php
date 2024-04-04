944f25f04e19acbf292b4720f243898f7d32b2a4
arielrecto <arielrecto29@gmail.com>
2024-04-04 11:33:07 +0800

    update:display no current event

--- resources/views/users/instructor/dashboard.blade.php
+++ resources/views/users/instructor/dashboard.blade.php
@@ -2,6 +2,8 @@
     <div class="flex flex-col gap-2 p-5">
         <div class="panel flex flex-col gap-2">
             <h1 class="panel-title">Current Event</h1>
+
+            @if($event === null)
             <div class="flex rounded-lg bg-blue-500 h-96 relative">
                 <img src="{{ $event->image }}" class="object-cover h-auto w-full" />
                 <a href="{{route('faculty.events.current', ['event' => $event->id])}}"
@@ -32,6 +34,16 @@
                     </div>
                 </a>
             </div>
+            @else
+            <div class="flex rounded-lg bg-blue-500 h-96 relative">
+                <img src="{{ asset('calendar.png') }}" class="object-cover h-auto w-full" />
+                <a href="#"
+                    class="w-full absolute z-10 bottom-0 h-24 flex items-center justify-center backdrop-blur-lg bg-white/30 p-5">
+                    No Current Event
+                </a>
+            </div>
+            @endif
+
         </div>
     </div>
 </x-dashboard.instructor>
