2387dacd1dcd758756f1703a9c9ea49922fa4448
arielrecto <arielrecto29@gmail.com>
2024-04-04 15:58:51 +0800

    update:test 2

--- app/Http/Middleware/EventAttendance.php
+++ app/Http/Middleware/EventAttendance.php
@@ -44,7 +44,7 @@ class EventAttendance
             ]);
         };
 
-        if ($end_date->lt($current_date)) {
+        if ($end_date->gt($current_date)) {
             return back()->with([
                 'message' => "The Event is Ended at {$start_date->format('F d, Y')}, The system process Attendance"
             ]);
