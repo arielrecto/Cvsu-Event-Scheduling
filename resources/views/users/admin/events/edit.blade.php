91642940cfc5ab0dc6574f09b95247e31d0ca682
arielrecto <arielrecto29@gmail.com>
2024-04-16 17:06:01 +0800

    update:edit event select tag change to select multiple

--- resources/views/users/admin/events/edit.blade.php
+++ resources/views/users/admin/events/edit.blade.php
@@ -18,7 +18,7 @@
 
                 </label>
                 <div class="flex items-center gap-2">
-                    <select name="speaker" class="select select-accent w-full">
+                    <select name="speakers"  multiple class="select select-accent w-full">
                         <option disabled selected>Select Host/Speaker</option>
                         @foreach ($speakers as $speaker)
                             <option value="{{ $speaker->id }}" class="capitalize">{{ $speaker->fullName() }}</option>
@@ -26,8 +26,8 @@
                     </select>
                     <a href="{{ route('speaker.create') }}" class="btn btn-accent">Add Event Speaker</a>
                 </div>
-                @if ($errors->has('speaker'))
-                    <p class="text-xs text-error">{{ $errors->first('speaker') }}</p>
+                @if ($errors->has('speakers'))
+                    <p class="text-xs text-error">{{ $errors->first('speakers') }}</p>
                 @endif
             </div>
             <div class="flex justify-center min-h-24" x-data="imageUploadHandler">
