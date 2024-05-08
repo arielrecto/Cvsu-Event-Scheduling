feb2ed28357fff11a930f623b392e011c90fee4d
arielrecto <arielrecto29@gmail.com>
2024-05-08 10:08:35 +0800

    update:event Archive

--- app/Http/Controllers/Admin/EventSpeakerController.php
+++ app/Http/Controllers/Admin/EventSpeakerController.php
@@ -162,7 +162,11 @@ class EventSpeakerController extends Controller
         $speaker = EventSpeaker::find($id);
 
 
-        $speaker->delete();
+        $speaker->update([
+            'is_archive' => true
+        ]);
+
+        // $speaker->delete();
 
 
         return back()->with(['message' => 'Event Speaker Delete Success']);
