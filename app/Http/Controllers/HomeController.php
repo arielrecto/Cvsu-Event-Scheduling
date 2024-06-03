9895147a88befb0fda3295b3588d84c9eb6d66bc
arielrecto <arielrecto29@gmail.com>
2024-06-03 11:23:45 +0800

    update:testing

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,6 +13,8 @@ class HomeController extends Controller
         $userRole = Auth::user()->roles()->first();
 
 
+        dd($userRole);
+
         switch($userRole->name){
             case UserRolesEnum::ADMIN->value:
                 return to_route('dashboard');
